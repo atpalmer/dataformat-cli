48051c2f0f05b2aa1888b44f9a5bd7038b7078c1
Andy Palmer <atp@sdf.org>
2019-05-04 13:02:11 -0500

    Add help messages for options

--- dataformat/main.py
+++ dataformat/main.py
@@ -7,8 +7,8 @@ from . import serde
 CHOICE = click.Choice(serde.CHOICES)
 
 
-@click.option('-o', '--out', 'out_format', default='json', type=CHOICE)
-@click.option('-i', '--in', 'in_format', default='json', type=CHOICE)
+@click.option('-o', '--out', 'out_format', default='json', type=CHOICE, help='Format of data read from STDIN')
+@click.option('-i', '--in', 'in_format', default='json', type=CHOICE, help='Format of data written to STDOUT')
 @click.command()
 def main(in_format, out_format):
     load, dump = serde.get(in_format, out_format)
